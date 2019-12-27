c83419ba5373df2c6a16d5c365951e3ecc1119e1
reema3 <31264341+reema3@users.noreply.github.com>
2019-12-27 21:57:05 +0530

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,19 @@
+library("data.table")
+setwd("C:\\Users\\Saurav Singla\\Documents\\Reema")
+
+#Load the data set
+electricity <- read.csv("Data1.csv")
+
+#Create Time Series field dateTime
+attach(electricity)
+electricity$dateTime = as.POSIXct(paste(Date, Time), format = "%d-%m-%Y %H:%M:%S")
+
+#Give the name and dimensions to your plot
+png("plot2.png", width=480, height=480)
+
+#Plot the histogram
+plot(x = dateTime
+     , y = Global_active_power
+     , type="l", xlab="", ylab="Global Active Power (kilowatts)")
+
+dev.off()

ca7c7834617df36f5c5317348e60deba8006b0e5
reema3 <31264341+reema3@users.noreply.github.com>
2019-12-29 13:34:30 +0530

    Add files via upload

--- project2/plot1.R
+++ project2/plot1.R
@@ -1,1 +1,14 @@
+setwd("C:\\Users\\Saurav Singla\\Documents\\Reema")
+#Load emission data
+Emission_Data <- readRDS("summarySCC_PM25.rds")
 
+#Load Source classification file
+SourceClass <- readRDS("Source_Classification_Code.rds")
+View(Emission_Data)
+View(SourceClass)
+table(Emission_Data$year)
+attach(Emission_Data)
+Agr=aggregate(Emissions, by=list(year), FUN='sum')
+barplot(Agr$x,Agr$Group.1, names = Agr$Group.1,
+        xlab = "Years", ylab = "Emissions"
+        , main = "Emissions over the Years")
